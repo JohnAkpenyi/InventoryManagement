91c138b96149865e7442bfdc29cdf3a3dc2220a1
The-RandomStuff <johnakpenyi@hotmail.com>
2023-04-24 16:20:18 +0100

    init commit

---
+++ src/main/java/com/capstone/InvetoryManagement/InvetoryManagementApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.InvetoryManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvetoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InvetoryManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/InvetoryManagement/InvetoryManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.InvetoryManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvetoryManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
